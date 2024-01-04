33d728e908db7abdb01f5772cb91ee99ef6acf5c
willer-613 <450673736@qq.com>
2024-01-04 13:26:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-najqgj
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/willer-613/vitejs-vite-najqgj)
---
+++ vue3-host-dynamic/src/components/IComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+import {
+  __federation_method_getRemote,
+  __federation_method_unwrapDefault,
+} from 'virtual:__federation__';
+
+const props = defineProps<{
+  dsl: Record<string, any>;
+}>();
+
+const com = defineAsyncComponent(async () => {
+  const module = await __federation_method_getRemote(
+    'remote-ui',
+    `./${props.dsl.type}`
+  );
+  return __federation_method_unwrapDefault(module);
+});
+</script>
+
+<template>
+  <component :is="com"></component>
+</template>
